f6ffbbd40e97a968a665453f4d080a8a0a3495a6
Abi Yoso <abiyosoa@gmail.com>
2019-08-23 02:28:39 +0700

    testimoni

--- application/views/testimoni.php
+++ application/views/testimoni.php
@@ -9,36 +9,6 @@
           <h3>Testimoni</h3>
           <p>Komentar dari beberapa klien kami mengenai layanan yang telah kami berikan</p>
         </header>
-<!--
-        <div class="row counters">
-
-  				<div class="col-lg-3 col-6 text-center">
-            <span data-toggle="counter-up">274</span>
-            <p>Clients</p>
-  				</div>
-
-          <div class="col-lg-3 col-6 text-center">
-            <span data-toggle="counter-up">421</span>
-            <p>Projects</p>
-  				</div>
-
-          <div class="col-lg-3 col-6 text-center">
-            <span data-toggle="counter-up">1,364</span>
-            <p>Hours Of Support</p>
-  				</div>
-
-          <div class="col-lg-3 col-6 text-center">
-            <span data-toggle="counter-up">18</span>
-            <p>Hard Workers</p>
-  				</div>
-
-  			</div>-->
-
-     <!--    <div class="facts-img">
-          <img src="assets/img/facts-img.png" alt="" class="img-fluid">
-        </div> -->
-
-
 
         <!--Section: Testimonials-->
 
