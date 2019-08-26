7f957b4f7fe9310f73f68fb573d1addd52dba7f0
Abi Yoso <abiyosoa@gmail.com>
2019-08-26 22:44:31 +0700

    edit banner

---
+++ application/views/home1.php
@@ -0,0 +1,17 @@
+
+<section id="intro">
+    <div class="intro-container">
+      <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner" role="listbox">
+    <div class="carousel-item active" style="background-image: url('assets/img/intro-carousel/bg-banner1.jpg');">
+      <!-- <img class="d-block w-100" src="assets/img/intro-carousel/bg-banner1.jpg"> -->
+    </div>
+    <!-- <div class="carousel-item">
+      <img class="d-block w-100" src="assets/img/intro-carousel/bg-banner1.jpg">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://mdbootstrap.com/img/Photos/Slides/img%20(31).jpg"> -->
+    </div>
+  </div>
+</div>
+</section>
---
+++ application/views/home2.php
@@ -0,0 +1,42 @@
+<section class="pb_cover_v3 overflow-hidden cover-bg-indigo cover-bg-opacity text-left pb_gradient_v1 pb_slant-light" id="section-home">
+<div class="container">
+<div class="row align-items-left justify-content-left">
+<div class="col-md-6">
+<h2 class="heading mb-3">Fly makes you faster</h2>
+<div class="sub-heading">
+<p class="mb-4">New free template by <a href="https://uicookies.com" target="_blank">uicookies.com</a>. For more templates visit the <a href="https://uicookies.com" target="_blank">site</a>. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+<p class="mb-5"><a class="btn btn-success btn-lg pb_btn-pill smoothscroll" href="#section-pricing"><span class="pb_font-14 text-uppercase pb_letter-spacing-1">See Pricing</span></a></p>
+</div>
+</div>
+<!-- <div class="col-md-1">
+</div>
+<div class="col-md-5 relative align-self-center">
+<form action="#" class="bg-white rounded pb_form_v1">
+<h2 class="mb-4 mt-0 text-center">Sign Up for Free</h2>
+<div class="form-group">
+<input type="text" class="form-control pb_height-50 reverse" placeholder="Full name">
+</div>
+<div class="form-group">
+<input type="text" class="form-control pb_height-50 reverse" placeholder="Email">
+</div>
+<div class="form-group">
+<input type="text" class="form-control pb_height-50 reverse" placeholder="Email">
+</div>
+<div class="form-group">
+<div class="pb_select-wrap">
+<select class="form-control pb_height-50 reverse">
+<option value="" selected="">Type</option>
+<option value="">Basic</option>
+<option value="">Business</option>
+<option value="">Free</option>
+</select>
+</div>
+</div>
+<div class="form-group">
+<input type="submit" class="btn btn-primary btn-lg btn-block pb_btn-pill  btn-shadow-blue" value="Register">
+</div>
+</form>
+</div> -->
+</div>
+</div>
+</section>
