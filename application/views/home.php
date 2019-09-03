d7dc0d70bdca6c09d6dfa82d7cc4c8ec35bbd2da
Abi Yoso <abiyosoa@gmail.com>
2019-09-03 22:30:14 +0700

    add-card

--- application/views/home.php
+++ application/views/home.php
@@ -11,7 +11,7 @@
 
           <div class="carousel-item active" style="background-image: url('assets/img/intro-carousel/bg-banner1.jpg');">
             
-            <div class="carousel-container">
+            <div class="">
               <div class="col-md-6">
                 <h3>Karena Langit Tidak <font color="#2DCC70">Selalu</font> Secerah Ini</h3>
                 <p>Semua memang terasa indah saat langit secerah ini, dan kita semua mengharapkan kecerahan ini berlangsung selamanya.</p>
@@ -20,20 +20,37 @@
                 
             
 
-            <div class="card" >
-                <!-- <img class="card-img" src="assets/img/card-1.png"> -->
+            <!-- <div class="card" >
                   <div class="card-body-simulasi">
-                    <h3 class="card-title">Simulasi</h3>
+                    <h4 class="card-title">Simulasi</h4>
                     <p class="card-text"> Kami akan bekerjasama dengan Anda untuk menjadikan aktivitas Perencanaan Keuangan sesuatu hal yang menyenangkan, mudah dipahami dan terjangkau oleh kemampuan Anda.</p>
                     <a href="#" class="btn btn-primary">Detail Info</a>
                   </div>
-                </div>
+            </div> -->
+
+            <div class="col-md-6">
+              
+            </div>
+
+            <div class="card">
+              <img class="card-img" src="assets/img/card-bg.png" alt="Card image" style="width: 70%;">
+                <div class="card-img-overlay">
+                  <h5 class="card-title">Simulasi</h5>
+                  <p class="card-text right">Kami akan bekerjasama dengan Anda untuk menjadikan aktivitas Perencanaan Keuangan sesuatu hal yang menyenangkan, mudah dipahami dan terjangkau oleh kemampuan Anda.</p>
+                  <a href="#" class="btn btn-success">Detail Info</a>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              
+            </div>
 
           </div>
         </div>
 
       </div>
     </div>
+  </div>
   </section><!-- #intro -->
 
   <main id="main">
